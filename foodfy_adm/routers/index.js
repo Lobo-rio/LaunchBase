b84fc2be30d5c64d5b96b7783882e4f29c308e68
Lobo-rio <gsilvamedeiros@gmail.com>
2020-06-27 19:44:01 -0300

    FoodFy Admin completo, 100% pronto!

--- foodfy_adm/routers/index.js
+++ foodfy_adm/routers/index.js
@@ -4,8 +4,13 @@ const foodfyAdmin = require('../controllers/index')
 
 
 router.get('/', foodfyAdmin.index)
-router.get('/create', foodfyAdmin.create)
-router.post('/create', foodfyAdmin.post)
 
+router.get('/admin/recipes', foodfyAdmin.index)
+router.get('/admin/recipes/create', foodfyAdmin.create)
+router.post('/admin/recipes', foodfyAdmin.post)
+router.get('/admin/recipes/:id', foodfyAdmin.show)
+router.get('/admin/recipes/:id/edit', foodfyAdmin.edit)
+router.put('/admin/recipes', foodfyAdmin.put)
+router.delete('/admin/recipes', foodfyAdmin.delete)
 
-module.exports = router
+module.exports = router 
--- foodfy_adm/server.js
+++ foodfy_adm/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const methodOverride = require('method-override')
 const router = require('./routers/index')
 
 const server = express()
 
+server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
+server.use(methodOverride('_method'))
 server.use(router)
+
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
@@ -13,9 +17,6 @@ nunjucks.configure("views", {
     autoescape: false,
     noCache: true
 })
-
-
-
 
 server.listen(3000, function(){
     console.log('Server Running port 3000!')
