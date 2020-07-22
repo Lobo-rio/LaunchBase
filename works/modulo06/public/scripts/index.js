eb9d5a873fb3b02c6a75670527017b9e2317195e
Lobo-rio <gsilvamedeiros@gmail.com>
2020-07-21 21:38:14 -0300

    Upload de images iniciado!

--- works/modulo06/public/scripts/index.js
+++ works/modulo06/public/scripts/index.js
@@ -13,3 +13,82 @@ const Mask = {
         }).format(value/100)
     }
 }
+
+const photosUpload = {
+    input: "",
+    preview: document.querySelector('#photos-preview'),
+    uploadLimit: 6,
+    files: [],
+    handleFileInput(event) {
+        
+        const { files: fileList } = event.target
+        photosUpload.input = event.target
+        
+        if (photosUpload.hasLimit(event)) return
+
+        Array.from(fileList).forEach(file => {
+
+            photosUpload.files.push(file)
+
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const div = photosUpload.getContainer(image)
+                photosUpload.preview.appendChild(div)
+            }
+            
+            reader.readAsDataURL(file)
+            
+        })
+
+        photosUpload.input.files = photosUpload.getAllFiles()
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick = photosUpload.removePhoto
+
+        div.appendChild(image)
+        div.appendChild(photosUpload.getRemoveButton())
+
+        return div
+    },
+    hasLimit(event) {
+        const { uploadLimit, input: fileList } = photosUpload
+
+        if ( fileList.length > uploadLimit ) {
+            alert(`Você atingiu o limite maximo de fotos, que são ${uploadLimit}`)
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+    getAllFiles() {
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        photosUpload.files.forEach(file => dataTransfer.itens.add(file))
+
+        return dataTransfer.files
+    },
+    getRemoveButton(){
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = 'close'
+        return button
+    },
+    removePhoto(event) {
+        const photoDiv = event.target.parentNode
+        const photosArray = Array.from(photosUpload.preview.children)
+        const index = photosArray.indexOf(photoDiv)
+
+        photosUpload.files.splice(index, 1)
+        photosUpload.input.files = photosUpload.getAllFiles()
+
+        photoDiv.remove()
+    }
+}
